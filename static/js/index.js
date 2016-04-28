37cb59d75435a034d9b2533dee4516b42d76b075
yawhide <ert.mcscrad@gmail.com>
2016-04-27 23:03:33 -0700

    added fautocomplete

--- static/js/index.js
+++ static/js/index.js
@@ -23,3 +23,7 @@ $form.onsubmit = function (e) {
   }
   localStorage['lol-personal-counter'] = JSON.stringify(storage);
 }
+
+/* autocomplete */
+var champions = ["Aatrox","Ahri","Akali","Alistar","Amumu","Anivia","Annie","Ashe","Aurelionsol","Azir","Bard","Blitzcrank","Brand","Braum","Caitlyn","Cassiopeia","Chogath","Corki","Darius","Diana","Draven","Drmundo","Ekko","Elise","Evelynn","Ezreal","Fiddlesticks","Fiora","Fizz","Galio","Gangplank","Garen","Gnar","Gragas","Graves","Hecarim","Heimerdinger","Illaoi","Irelia","Janna","Jarvaniv","Jax","Jayce","Jhin","Jinx","Kalista","Karma","Karthus","Kassadin","Katarina","Kayle","Kennen","Khazix","Kindred","Kogmaw","Leblanc","Leesin","Leona","Lissandra","Lucian","Lulu","Lux","Malphite","Malzahar","Maokai","Master yi","Missfortune","Monkeyking","Mordekaiser","Morgana","Nami","Nasus","Nautilus","Nidalee","Nocturne","Nunu","Olaf","Orianna","Pantheon","Poppy","Quinn","Rammus","Reksai","Renekton","Rengar","Riven","Rumble","Ryze","Sejuani","Shaco","Shen","Shyvana","Singed","Sion","Sivir","Skarner","Sona","Soraka","Swain","Syndra","Tahmkench","Talon","Taric","Teemo","Thresh","Tristana","Trundle","Tryndamere","Twistedfate","Twitch","Udyr","Urgot","Varus","Vayne","Veigar","Velkoz","Vi","Viktor","Vladimir","Volibear","Warwick","Wukong","Xerath","Xinzhao","Yasuo","Yorick","Zac","Zed","Ziggs","Zilean","Zyra"];
+
