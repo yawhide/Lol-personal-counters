156eba6a10bcedd72fe2f46da725f81e43c42bd2
Ooblah <thewonderbird@hotmail.com>
2016-05-07 16:11:55 -0400

    matchup page general styling changes

--- static/js/style.js
+++ static/js/style.js
@@ -1,8 +1,13 @@
 window.onload = function() {
+	var bgC = new RGBA(255, 255, 255, 0);
+
 	var champList = document.getElementById("championList").getElementsByTagName("tr");
 	for (var i = champList.length - 1; i > 0; i--) {
 		var percent = parseInt(champList[i].children[1].innerText);
 		setBG(champList[i], percent);
+		if (i == 2) bgC = new RGBA(255, 245, 153, 0);
+		if (i == 3) bgC = new RGBA(188, 219, 246, 0);
+		if (i == 4) bgC = new RGBA(169, 239, 222, 0);
 	}
 
 	function RGBA(red, green, blue, alpha) {
@@ -16,7 +21,6 @@ window.onload = function() {
 	}
 
 	function setBG(elem, opac) {
-		var bgC = new RGBA(255, 255, 255, 0);
 		bgC.alpha = opac/2/100;
 		elem.style.backgroundColor = bgC.getCSS();
 	}
